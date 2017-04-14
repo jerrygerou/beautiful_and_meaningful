2ebdc802fbbe278446305a01abb0f801e7b19be0
Sara Gerou <saragerou@gmail.com>
2017-04-14 15:57:23 -0500

    Updates

--- public/js/application.js
+++ public/js/application.js
@@ -1,4 +1,20 @@
-$(document).ready(function() {
+// $(document).ready(function() {
+//   $(".container").on("submit", ".action_buttons", function() {
+//     event.preventDefault();
+//     console.log("poo");
 
+//     var $form = $(this);
+//     // console.log($form);
+//     $.ajax({
+//       url: $form.attr("action"),
+//       method: $form.attr("method"),
 
-});
+//     })
+//     .done(function(response) {
+
+//       console.log(response);
+//     });
+
+//   });
+
+// });
